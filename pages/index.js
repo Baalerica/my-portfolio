55fc350b5401171fadbde9174a8a132510a68f2d
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-05-22 17:47:26 -0600

    Add &apos;

--- pages/index.js
+++ pages/index.js
@@ -250,7 +250,7 @@ export default function Home() {
       </p>
 
       <h2 className="text-center font-header text-4xl font-semibold uppercase text-primary sm:text-5xl lg:text-6xl">
-        Here's what I'm good at
+        Here&apos;s what I&apos;m good at
       </h2>
       <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
         These are the services I offer
@@ -260,7 +260,7 @@ export default function Home() {
           Check out my Portfolio
         </h2>
         <h3 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
-          Here's what I have done with the past
+          Here&apos;s what I have done with the past
         </h3>
 
         <div classNameName="mx-auto grid w-full grid-cols-1 gap-8 pt-12 sm:w-3/4 md:gap-10 lg:w-full lg:grid-cols-2">
