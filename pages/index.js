dd17a5200fac36489b705e87b2a937d66f161727
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-05-22 21:32:33 -0600

    Add experience baner

--- pages/index.js
+++ pages/index.js
@@ -483,6 +483,99 @@ export default function Home() {
         </div>
       </div>
 
+
+      <div className="bg-cover bg-top bg-no-repeat pb-16 md:py-16 lg:py-24"
+        style={{ backgroundImage: 'url(/experience-figure.png)' }}
+        id="statistics">
+        <div className="container">
+          <div className="mx-auto w-5/6 bg-white py-16 shadow md:w-11/12 lg:py-20 xl:py-24 2xl:w-full">
+            <div className="grid grid-cols-2 gap-5 md:gap-8 xl:grid-cols-4 xl:gap-5">
+              <div className="flex flex-col items-center justify-center text-center md:flex-row md:text-left">
+                <div className="w-32">
+                  <Image
+                    src="/logo-citc.png"
+                    alt="client logo"
+                    width={20}
+                    height={20}
+                    className="mx-auto block h-12 w-auto"
+                  />
+                </div>
+                <div className="pt-5 md:pl-5 md:pt-0">
+                  <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
+                    5
+                  </h1>
+                  <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
+                    Participation in hackathons
+                  </h4>
+                </div>
+              </div>
+
+              <div class="flex flex-col items-center justify-center text-center md:flex-row md:text-left">
+                <div>
+                  <Image
+                    src="/logo-citc.png"
+                    alt="client logo"
+                    width={20}
+                    height={20}
+                    className="mx-auto block h-12 w-auto"
+                  />
+                </div>
+                <div className="pt-5 md:pl-5 md:pt-0">
+                  <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
+                    2
+                  </h1>
+                  <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
+                    School competitions won
+                  </h4>
+                </div>
+              </div>
+
+              <div
+                className="mt-6 flex flex-col items-center justify-center text-center md:mt-10 md:flex-row md:text-left lg:mt-0">
+                <div>
+                  <Image
+                    src="/logo-citc.png"
+                    alt="client logo"
+                    width={20}
+                    height={20}
+                    className="mx-auto block h-12 w-auto"
+                  />
+                </div>
+                <div className="pt-5 md:pl-5 md:pt-0">
+                  <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
+                    8
+                  </h1>
+                  <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
+                    Happy Clients
+                  </h4>
+                </div>
+              </div>
+
+              <div
+                className="mt-6 flex flex-col items-center justify-center text-center md:mt-10 md:flex-row md:text-left lg:mt-0">
+                <div>
+                  <Image
+                    src="/logo-citc.png"
+                    alt="client logo"
+                    width={20}
+                    height={20}
+                    className="mx-auto block h-12 w-auto"
+                  />
+                </div>
+                <div className="pt-5 md:pl-5 md:pt-0">
+                  <h1 className="font-body text-2xl font-bold text-primary md:text-4xl">
+                    99
+                  </h1>
+                  <h4 className="text-grey-dark font-header text-base font-medium leading-loose md:text-xl">
+                    Bugs Fixed
+                  </h4>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </>
   );
 }
