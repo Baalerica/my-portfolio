edd3cc719b54ed23e12e383f977dd55d952161ec
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-05-24 13:03:33 -0600

    Hidden menu

--- pages/index.js
+++ pages/index.js
@@ -26,24 +26,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
 
-      <div className="h-screen w-full"
-        style={{ backgroundImage: "url(/fondo-azul.jpg)", backgroundSize: 'cover', backgroundPosition: 'center' }}
+      <div
+        className="h-screen w-full"
+        style={{
+          backgroundImage: "url(/fondo-azul.jpg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
       >
         <header className="absolute inset-x-0 top-0 z-50">
           <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Baalerica</span>
-                <Image
-                  className="h-8 w-auto"
-                  src="/logo.png"
-                  alt=""
-                  width={32}
-                  height={32}
-                />
+                <Image className="h-8 w-auto" src="/logo.png" alt="" width={32} height={32} />
               </a>
             </div>
-            <div className="flex lg:hidden">
+            <div className="flex lg:hidden sm:hidden">
               <button
                 type="button"
                 className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
@@ -57,11 +56,7 @@ export default function Home() {
                   stroke="currentColor"
                   aria-hidden="true"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
               </button>
             </div>
@@ -109,13 +104,7 @@ export default function Home() {
               <div className="flex items-center justify-between">
                 <a href="#" className="-m-1.5 p-1.5">
                   <span className="sr-only">Baalerica</span>
-                  <Image
-                    className="h-8 w-auto"
-                    src="/logo.png"
-                    alt=""
-                    width={32}
-                    height={32}
-                  />
+                  <Image className="h-8 w-auto" src="/logo.png" alt="" width={32} height={32} />
                 </a>
 
                 <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
@@ -183,7 +172,6 @@ export default function Home() {
                           Hire me! <span aria-hidden="true">&rarr;</span>
                         </a>
                       </Link>
-
                     </div>
                   </div>
                   <div className="py-6">
@@ -192,7 +180,6 @@ export default function Home() {
                         Log in <span aria-hidden="true">&rarr;</span>
                       </a>
                     </Link>
-
                   </div>
                 </div>
               </div>
