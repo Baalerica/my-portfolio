ee74da27f58d9f8b5435530172d93c51985578b0
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-05-30 16:43:09 -0600

    Add auth/error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,32 +1,31 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongodb";
 
-
 export const authOptions = {
-    // Configure one or more authentication providers
+    // Configura uno o más proveedores de autenticación
     adapter: MongoDBAdapter(clientPromise),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
         }),
-
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-
-
-        // ...add more providers here
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        }),
+        // Puedes añadir más proveedores aquí
     ],
-
     debug: true,
     pages: {
-        error: '/auth/error', // Redirige a esta página en caso de error
+        signIn: '/auth/signin',
+        signOut: '/auth/signout',
+        error: '/auth/error',
+        verifyRequest: '/auth/verify-request',
+        newUser: "https://my-portfolio-eosin-pi.vercel.app/api/auth/error"
     },
-}
+};
 
-export default NextAuth(authOptions)
+export default NextAuth(authOptions);
