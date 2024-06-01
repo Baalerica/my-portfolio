3d036c06f88bb3d555bdf26c7090f50c28112a9e
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-06-01 12:42:42 -0600

    Add google an git callback

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,16 +4,35 @@ import GoogleProvider from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongodb";
 
+// Definir las URLs de callback según el entorno
+const googleCallbackURL = process.env.NODE_ENV === 'production'
+    ? 'https://tu-app.vercel.app/api/auth/callback/google'
+    : 'http://localhost:3000/api/auth/callback/google';
+
+const githubCallbackURL = process.env.NODE_ENV === 'production'
+    ? 'https://tu-app.vercel.app/api/auth/callback/github'
+    : 'http://localhost:3000/api/auth/callback/github';
+
 export const authOptions = {
     adapter: MongoDBAdapter(clientPromise),
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
+            authorization: {
+                params: {
+                    redirect_uri: githubCallbackURL,
+                }
+            }
         }),
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            authorization: {
+                params: {
+                    redirect_uri: googleCallbackURL,
+                }
+            }
         }),
     ],
     debug: true,
@@ -27,4 +46,3 @@ export const authOptions = {
 };
 
 export default NextAuth(authOptions);
-
