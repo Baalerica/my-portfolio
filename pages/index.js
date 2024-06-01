4bef0bf607cedfb8728d45e8b2bb3bef935c27dc
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-05-31 19:19:36 -0600

    Try to fix the cell phone menu

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,12 @@ import Head from "next/head";
 import Link from 'next/link';
 import Image from "next/image";
 import React from "react";
-import {
-  FacebookShareButton,
-  FacebookIcon,
-  PinterestShareButton,
-  PinterestIcon,
-  RedditShareButton,
-  RedditIcon,
-  WhatsappShareButton,
-  WhatsappIcon,
-  LinkedinShareButton,
-  LinkedinIcon,
-} from "next-share";
-
+import { useState } from "react";
 
 export default function Home() {
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
       <Head>
@@ -98,93 +89,141 @@ export default function Home() {
               </Link>
             </div>
           </nav>
-          <div className="lg:hidden" role="dialog" aria-modal="true">
-            <div className="fixed inset-0 z-50"></div>
-            <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-              <div className="flex items-center justify-between">
-                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Baalerica</span>
-                  <Image className="h-8 w-auto" src="/logo.png" alt="" width={32} height={32} />
-                </a>
 
-                <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-                  <span className="sr-only">Close menu</span>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-              <div className="mt-6 flow-root">
-                <div className="-my-6 divide-y divide-gray-500/10">
-                  <div className="space-y-2 py-6">
-                    <a
-                      href="#about"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                    >
-                      About
+          <button
+            type="button"
+            className="lg:hidden p-2 text-gray-700"
+            onClick={() => setMenuOpen(true)}
+          >
+            <span className="sr-only">Open menu</span>
+            <svg
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              aria-hidden="true"
+            >
+              <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
+            </svg>
+          </button>
+
+
+
+          <div>
+            <button
+              type="button"
+              className="lg:hidden p-2 text-gray-700"
+              onClick={() => setMenuOpen(true)}
+            >
+              <span className="sr-only">Open menu</span>
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 5.75h16.5M3.75 12h16.5M3.75 18.25h16.5" />
+              </svg>
+            </button>
+
+            {menuOpen && (
+              <div className="lg:hidden" role="dialog" aria-modal="true">
+                <div className="fixed inset-0 z-50 bg-black opacity-50" onClick={() => setMenuOpen(false)}></div>
+                <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                  <div className="flex items-center justify-between">
+                    <a href="#" className="-m-1.5 p-1.5">
+                      <span className="sr-only">Baalerica</span>
+                      <Image className="h-8 w-auto" src="/logo.png" alt="" width={32} height={32} />
                     </a>
-                    <a
-                      href="#services"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
+
+                    <button
+                      type="button"
+                      className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                      onClick={() => setMenuOpen(false)}
                     >
-                      Services
-                    </a>
-                    <a
-                      href="#portfolio"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                    >
-                      Portfolio
-                    </a>
-                    <a
-                      href="#clients"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                    >
-                      Clients
-                    </a>
-                    <a
-                      href="#work"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                    >
-                      Work
-                    </a>
-                    <a
-                      href="#statistics"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                    >
-                      Statistics
-                    </a>
-                    <a
-                      href="#contact"
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-50"
-                    >
-                      Projects
-                    </a>
-                    <div className="py-6">
-                      <Link href="/stripe" legacyBehavior>
-                        <a className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50">
-                          Hire me! <span aria-hidden="true">&rarr;</span>
-                        </a>
-                      </Link>
-                    </div>
+                      <span className="sr-only">Close menu</span>
+                      <svg
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    </button>
                   </div>
-                  <div className="py-6">
-                    <Link href="/login" legacyBehavior>
-                      <a className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-50">
-                        Log in <span aria-hidden="true">&rarr;</span>
-                      </a>
-                    </Link>
+                  <div className="mt-6 flow-root">
+                    <div className="-my-6 divide-y divide-gray-500/10">
+                      <div className="space-y-2 py-6">
+                        <a
+                          href="#about"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          About
+                        </a>
+                        <a
+                          href="#services"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Services
+                        </a>
+                        <a
+                          href="#portfolio"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Portfolio
+                        </a>
+                        <a
+                          href="#clients"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Clients
+                        </a>
+                        <a
+                          href="#work"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Work
+                        </a>
+                        <a
+                          href="#statistics"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Statistics
+                        </a>
+                        <a
+                          href="#contact"
+                          className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                          Projects
+                        </a>
+                        <div className="py-6">
+                          <Link href="/stripe" legacyBehavior>
+                            <a className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                              Hire me! <span aria-hidden="true">&rarr;</span>
+                            </a>
+                          </Link>
+                        </div>
+                      </div>
+                      <div className="py-6">
+                        <Link href="/login" legacyBehavior>
+                          <a className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                            Log in <span aria-hidden="true">&rarr;</span>
+                          </a>
+                        </Link>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
+            )}
           </div>
+
         </header>
         <div className="relative isolate px-6 pt-14 lg:px-8">
           <div
