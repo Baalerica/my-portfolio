76dcec6843cbdacd2e661676744016545293d9b9
Erica Rodríguez <erica_rdzb@hotmail.com>
2024-06-03 11:22:29 -0600

    Fix cell phone menu 5

--- pages/index.js
+++ pages/index.js
@@ -33,24 +33,7 @@ export default function Home() {
                 <Image className="h-8 w-auto" src="/logo.png" alt="" width={32} height={32} />
               </a>
             </div>
-            <div className="flex lg:hidden sm:hidden">
-              <button
-                type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-              >
-                <span className="sr-only">Open main menu</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                </svg>
-              </button>
-            </div>
+
             <div className="hidden lg:flex lg:gap-x-12">
               <a href="#about" className="text-sm font-semibold leading-6 text-white">
                 About
@@ -119,6 +102,23 @@ export default function Home() {
                       <Image className="h-8 w-auto" src="/logo.png" alt="" width={32} height={32} />
                     </a>
 
+                    <button
+                      type="button"
+                      className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                      onClick={() => setMenuOpen(false)}
+                    >
+                      <span className="sr-only">Close menu</span>
+                      <svg
+                        className="h-6 w-6"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                      >
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                      </svg>
+                    </button>
                   </div>
                   <div className="mt-6 flow-root">
                     <div className="-my-6 divide-y divide-gray-500/10">
